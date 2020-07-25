e82031fb3406c25a5808574ff55aaefe7d4129ac
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-25 21:24:57 +0000

    fix: what happens when head is null

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -12,12 +12,13 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *tmp = *head;
 	listint_t *tmp2;
 	listint_t *new = malloc(sizeof(listint_t));
+
 	if (new == NULL || !number )
 		return NULL;
 	new->n = number;
 	new->next = NULL;
 	if (head == NULL)
-		head = new;
+		*head = new;
 	while (tmp != NULL && tmp->n < number)
 	{
 		tmp2 = tmp;
