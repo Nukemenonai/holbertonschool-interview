349c0ed9c8fcce7359039bf4bf08360440ff19f9
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-04 22:59:33 +0000

    fix: betty format

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -22,13 +22,6 @@ typedef struct binary_tree_s
 
 typedef struct binary_tree_s heap_t;
 
-typedef struct Queue
-{
-	int size;
-	int front, rear;
-	heap_t **array;
-} Queue;
-
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *create(int value);
