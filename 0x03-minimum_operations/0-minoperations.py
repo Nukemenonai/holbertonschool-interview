efeb9f7f6509678d5f027cc27ce7b689ee05198d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-09 04:11:47 +0000

    progress on task 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -15,20 +15,15 @@ def _isprime(n):
             return True
 
 
-def ops_registry(li, n, lenchar, last_batch_copied):
-    """ dynamic programming algo"""
-    if lenchar >= n:
-        return
-    if lenchar <= int(n / 2):
-        li.append('cpa')
-        li.append('pt')
-        last_batch_copied = lenchar
-        lenchar += last_batch_copied
-        ops_registry(li, n, lenchar, last_batch_copied)
-    else:
-        li.append('pt')
-        lenchar += last_batch_copied
-        ops_registry(li, n, lenchar, last_batch_copied)
+def find_ld(n):
+    """finds the largest divisor of n and the largest of each subsequent"""
+    div_li = []
+    while n > 1:
+        for i in range(2, n):
+            if n % i == 0:
+                div_li.append(n / i)
+                n = int(n / i)
+    return sorted(div_li)
 
 
 def minOperations(n):
@@ -39,6 +34,15 @@ def minOperations(n):
     if _isprime(n) is True:
         return n
     else:
-        li = []
-        ops_registry(li, n, 1, 0)
-        return len(li)
+        lenchar = 1
+        ops_li = []
+        div_li = find_ld(n)
+        i = 0
+        ops_li.append('cp_all')
+        while (lenchar != n):
+            ops_li.append('paste')
+            lenchar += div_li[i]
+            if i < len(div_li) - 1 and lenchar == div_li[i + 1] and lenchar < n:
+                i += 1
+                ops_li.append('cp_all')
+    return len(ops_li)
