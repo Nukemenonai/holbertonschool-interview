7ce6287615ce1f14d4a2302077e204850770e9b0
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-09 16:43:18 +0000

    i am done

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,27 +3,27 @@
 this file contains the min operations algorithm
 """
 
-
 def _isprime(n):
-    """ checks for primality of n"""
-    if n < 2:
+    if n > 1:
+        for i in range(2,n):
+            if (n % i) == 0:
+                return False
+        return True
+    else:
         return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
         else:
-            return True
-
-
-def find_ld(n):
-    """finds the largest divisor of n and the largest of each subsequent"""
-    div_li = []
-    while n > 1:
-        for i in range(2, n):
-            if n % i == 0:
-                div_li.append(n / i)
-                n = int(n / i)
-    return sorted(div_li)
+            factors.append(n // i)
+            n //= i
+    if n > 1:
+        factors.append(n // i)
+    return sorted(factors)
 
 
 def minOperations(n):
@@ -36,7 +36,7 @@ def minOperations(n):
     else:
         lenchar = 1
         ops_li = []
-        div_li = find_ld(n)
+        div_li = prime_factors(n)
         i = 0
         ops_li.append('cp_all')
         while (lenchar != n):
