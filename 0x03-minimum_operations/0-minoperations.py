1f2018de5e7ef91a2eb54a8b084041bd43f8a9b4
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-10 22:49:10 +0000

    minops

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,46 +3,19 @@
 this file contains the min operations algorithm
 """
 
-def _isprime(n):
-    if n > 1:
-        for i in range(2,n):
-            if (n % i) == 0:
-                return False
-        return True
-    else:
-        return False
-
-def prime_factors(n):
-    i = 2
-    factors = []
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            factors.append(n // i)
-            n //= i
-    if n > 1:
-        factors.append(n // i)
-    return sorted(factors)
-
 
 def minOperations(n):
     """calculates the fewest number of operations
     needed to result in exactly n H characters """
     if type(n) != int or n <= 1:
         return 0
-    if _isprime(n) is True:
-        return n
-    else:
-        lenchar = 1
-        ops_li = []
-        div_li = prime_factors(n)
-        i = 0
-        ops_li.append('cp_all')
-        while (lenchar != n):
-            ops_li.append('paste')
-            lenchar += div_li[i]
-            if i < len(div_li) - 1 and lenchar == div_li[i + 1] and lenchar < n:
-                i += 1
-                ops_li.append('cp_all')
-    return len(ops_li)
+
+    nops = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            n //= i
+            nops += i
+        else:
+            i += 1
+    return nops
