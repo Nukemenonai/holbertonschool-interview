c948e2d278dd6fecbfdd12d5c755493eb52e168d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-13 04:38:45 +0000

    progress on task 0

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
+void stabilizer(int grid1[3][3], int grid2[3][3]);
+void sum_piles(int grid1[3][3], int grid2[3][3]);
+int checker(int grid[3][3]);
+
+
+#endif
