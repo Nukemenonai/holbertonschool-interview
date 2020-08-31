4d5f9f2c498d2ace3eaa398c78ae3495daf72ed0
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-31 00:20:37 +0000

    log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -16,22 +16,22 @@ dict = {'200': 0,
 
 try:
     for line in sys.stdin:
-        i += 1
-        try:
-            size += int(line.split()[-1])
-        except:
-            pass
-        try:
-            if str(line.split()[7]) in dict.keys():
-                dict[str(line.split()[7])] += 1
-        except:
-            pass
         try:
             if i != 0 and i % 10 == 0:
                 print("File size: {}".format(size))
                 for key in sorted(dict.keys()):
                     if dict[key] != 0:
                         print("{}: {}".format(key, dict[key]))
+        except:
+            pass
+        try:
+            size += int(line.split()[-1])
+        except:
+            pass
+        i += 1
+        try:
+            if str(line.split()[7]) in dict.keys():
+                dict[str(line.split()[7])] += 1
         except:
             pass
 except KeyboardInterrupt:
