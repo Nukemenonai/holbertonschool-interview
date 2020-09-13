5e17537b9ed0c46dc896fd04698402dcd1cefa68
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-13 16:08:14 +0000

    checks if a number is a palindrome

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "palindrome.h"
+
+/**
+ * reverse - reverses a number
+ * @n: number to reverse
+ *
+ * Return: reversed number
+ */
+
+unsigned long reverse(unsigned long n, unsigned long res,
+	    unsigned long remainder, unsigned long quot)
+{
+
+	remainder = n%10;
+	res = (res * 10) + remainder;
+	quot = quot/10;
+	if (quot == 0)
+		return res;
+	else
+		return reverse(quot, res, 0, quot);
+}
+
+
+/**
+ * is_palindrome - checks if a number is a palindrome
+ *
+ * @n: the number to be checked
+ *
+ * Return: 1 if n is palindrome 0 otherwise
+ *
+ */
+
+int is_palindrome(unsigned long n)
+{
+	unsigned long reversed;
+	unsigned long res = 0;
+	unsigned long remainder = 0;
+	unsigned long quot = n;
+
+	reversed = reverse(n, res, remainder, quot);
+
+	return (reversed == n ? 1: 0);
+}
---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,9 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+
+int is_palindrome(unsigned long n);
+unsigned long reverse(unsigned long n, unsigned long res,
+	    unsigned long remainder, unsigned long quot);
+
+#endif
