edeb4b3f6cd0dc4850891ccd92e33c5d5bea9cfd
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-20 16:37:34 +0000

    checks if a list of integers is a valid UTF-8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+utf validator
+"""
+
+
+def validUTF8(data):
+    """
+    checks if an array if integers representing a UTF-8 is valid UTF-8
+    data: list of integers
+    returns: true if valid else 0
+    """
+    binary = []
+    for number in data:
+        binary.append("{:08b}".format(number))
+    for byte in binary:
+        if int(byte[0]) == 0:
+            continue
+        ones = byte.count('1')
+        if ones >= 4 or ones <= 1:
+            return False
+        for _ in range(ones - 1):
+            try:
+                byte = next(binary)
+            except StopIteration:
+                return False
+            if byte[0:2] != [1, 0]:
+                return False
+    return True
