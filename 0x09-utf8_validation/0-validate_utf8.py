648eb45a122f74cb6512b4697b5a23fb9b09866f
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-24 04:54:42 +0000

    checks if a list of integers is a valid UTF-8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -16,12 +16,13 @@ def validUTF8(data):
         bin_rep = format(num, '#010b')[-8:]
         if n_bytes == 0:
             for bit in bin_rep:
-                if bit == '0': break
+                if bit == '0':
+                    break
                 n_bytes += 1
             if n_bytes == 0:
                 continue
             if n_bytes == 1 or n_bytes > 4:
-                    return False
+                return False
         else:
             if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
