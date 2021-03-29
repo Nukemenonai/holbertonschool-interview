3be91efd01a893b4ba73209e909b68c9cc11ea7b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-03-29 17:26:31 -0500

    fix: betty style

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,11 +1,11 @@
 #include "search.h"
 
 /**
-* linear-skip - searches for a value in a sorted skip list
+* linear_skip - searches for a value in a sorted skip list
 * @list: pointer to the head of of the skip list
 * @value: the value to look for
 *
-* Return :a pointer to the first nod where value is located 
+* Return: a pointer to the first nod where value is located
 */
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
@@ -27,12 +27,12 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 				pos->index, pos->n);
 		}
 		else
-			while(pos->next)
+			while (pos->next)
 				pos = pos->next;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n",
 		min->index, pos->index);
-	
+
 	for (i = pos->index;
 		i <= (pos->index) && (min->n <= value);
 		i++, min = min->next)
