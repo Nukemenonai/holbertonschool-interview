7cc3e6192f040a01cc903f56fe766fc74e54f69c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-06-02 08:55:52 -0500

    find the loop iin a linked list

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+* find_listint_loop - finds the loop in a linked list
+* using the floyd agorithm
+* @head: the pointer to the head of the linked list
+*
+* Return: pointer where loop starts or NULL if no loop 
+**/
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *hare = NULL;
+	listint_t *tortoise = NULL; 
+
+	if (!head)
+		return (NULL);
+
+	hare = head;
+	tortoise = head; 
+
+	while(tortoise && hare && hare->next)
+	{
+		hare = (hare->next)->next;
+		tortoise = tortoise->next;
+
+		if (hare == tortoise)
+		{
+			return tortoise
+		}
+	}
+	
+	return (NULL);
+
+}
