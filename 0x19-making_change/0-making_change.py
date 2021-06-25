290c0728261e586507a4e02ff8f71584b07c534b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-06-24 21:42:18 -0500

    fix: opted for division of total between denominations

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Making change 
+Making change
 """
+
 
 def makeChange(coins, total):
     """ Given a pile of coins of different values,
@@ -12,24 +13,20 @@ def makeChange(coins, total):
         coins: list of the values of the coins in your possession
         total: total to be met
     """
-    sum = 0
-    i = 0
-    count = 0
-
     if total <= 0:
         return 0
 
     sorted_coins = sorted(coins, reverse=True)
-    n_denom = len(sorted_coins)
-    aux = total - sum
+    amt = 0
 
-    while sum < total and i < n_denom:
-        while s_coins[i] <= aux:
-            sum += sorted_coins[i]
-            count += 1
-            if sum == total:
-                return count
-            aux = total - sum
-        i += 1
+    for coin in sorted_coins:
+        if total <= 0:
+            break
+        aux = total // coin
+        amt += aux
+        total -= (aux * coin)
 
-    return -1
+    if total != 0:
+        return -1
+
+    return amt
