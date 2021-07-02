ee401c8b97e001585d5820bbffada00b5a5ce491
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-07-01 22:03:58 -0500

    add a node to the end of a double circular linked list

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -6,45 +6,45 @@
 /**
  * add_node_end - Adds a new node to the end of a double circular linked list
  *
- * @list:the list to modify
- * @str: the string to copy into the new node
- * Return: Address of the new node, or NULL on failure
+ * @list:the linked list to be modified modify
+ * @str: the string to be copied in the new node
+ * Return: Address of the new node, NULL on failure
  */
 List *add_node_end(List **list, char *str)
 {
-	List *new_node;
+	List *new;
 	char *strNode;
 
 	if (!list)
 		return (NULL);
 
-	new_node = malloc(sizeof(List));
-	if (!new_node)
+	new = malloc(sizeof(List));
+	if (!new)
 		return (NULL);
 
 	strNode = strdup(str);
 	if (!strNode)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
-	new_node->str = strNode;
+	new->str = strNode;
 
 	if (!*list)
 	{
-		new_node->next = new_node;
-		new_node->prev = new_node;
-		*list = new_node;
-		return (new_node);
+		new->next = new;
+		new->prev = new;
+		*list = new;
+		return (new);
 	}
 
-	new_node->next = *list;
-	new_node->prev = (*list)->prev;
+	new->next = *list;
+	new->prev = (*list)->prev;
 
-	(*list)->prev = new_node;
-	new_node->prev->next = new_node;
+	(*list)->prev = new;
+	new->prev->next = new;
 
-	return (new_node);
+	return (new);
 }
 /**
  * add_node_begin - Adds a new node to the beginning of a
@@ -56,39 +56,39 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-	List *new_node;
+	List *new;
 	char *strNode;
 
 	if (!list)
 		return (NULL);
 
-	new_node = malloc(sizeof(List));
-	if (!new_node)
+	new = malloc(sizeof(List));
+	if (!new)
 		return (NULL);
 
 	strNode = strdup(str);
 	if (!strNode)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
-	new_node->str = strNode;
+	new->str = strNode;
 
 	if (*list)
 	{
-		new_node->next = *list;
-		new_node->prev = (*list)->prev;
-		(*list)->prev = new_node;
-		if (new_node->prev)
-			new_node->prev->next = new_node;
+		new->next = *list;
+		new->prev = (*list)->prev;
+		(*list)->prev = new;
+		if (new->prev)
+			new->prev->next = new;
 	}
 	else
 	{
-		new_node->prev = new_node;
-		new_node->next = new_node;
+		new->prev = new;
+		new->next = new;
 	}
 
-	*list = new_node;
+	*list = new;
 
-	return (new_node);
+	return (new);
 }
